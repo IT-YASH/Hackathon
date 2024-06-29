87ed4961e18977e3572ed68935c5cb4752feaf43
IT-YASH <pyash7646@gmail.com>
2024-06-29 18:52:05 +0530

    first commit

--- src/Components/About/OurTeam.jsx
+++ src/Components/About/OurTeam.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import Deepak from "../Images/Deepak.jpg";
 import Rohit from "../Images/Rohit.png";
-import Raushan from "../Images/Raushan.png";
-import Heena from "../Images/Heena.png";
-import Poornima from "../Images/Poornima.jpg"
+// import Raushan from "../Images/Raushan.png";
+// import Heena from "../Images/Heena.png";
+// import Poornima from "../Images/Poornima.jpg"
 
 function OurTeam() {
   return (
@@ -39,45 +39,8 @@ function OurTeam() {
           </div>
         </div>
         <div>
-          <div>
-            <img
-              src= {Heena}
-              className="hover:scale-105 transition-transform image3"
-            />
-          </div>
-          <div className="deepak">
-            <h4 className="text-2xl py-3 md:py-2 font-medium">Heena Parveen</h4>
-            <p className="text-xs font-base text-gray-400">Full-stack developer</p>
-          </div>
+          
         </div>
-        <div className="rrr">
-          <div>
-            <img
-              src={Raushan}
-              className="hover:scale-105 transition-transform image4"
-            />
-          </div>
-          <div className="iii">
-            <h4 className="text-2xl py-3 md:py-2 font-medium">Raushan Raj Singh	</h4>
-            <p className="text-xs font-base text-gray-400">
-            Full-stack developer
-            </p>
-          </div>
-        </div>
-        <div>
-          <div>
-            <img
-              src={Poornima}
-              className="hover:scale-105 transition-transform image5"
-            />
-          </div>
-          <div className="oooo">
-            <h4 className="text-2xl py-3 md:py-2 font-medium">Poornima Mohabe</h4>
-            <p className="text-xs font-base text-gray-400">
-            Full-stack developer
-            </p>
-          </div>
-        </div> 
       </div>
     </div>
   );
